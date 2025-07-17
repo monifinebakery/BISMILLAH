cf37a564e39c0a74c44095e8ba37dcbea96b8c9c
monifinebakery <monifinebakery@gmail.com>
2025-07-18 00:49:00 +0700

    Updated AppDataContext.tsx

--- src/contexts/AppDataContext.tsx
+++ src/contexts/AppDataContext.tsx
@@ -569,3 +569,11 @@ export const useSupabaseSync = () => {
     isLoading
   };
 };
+
+export const useAppData = () => {
+  const context = useContext(AppDataContext);
+  if (context === undefined) {
+    throw new Error('useAppData must be used within an AppDataProvider');
+  }
+  return context;
+};

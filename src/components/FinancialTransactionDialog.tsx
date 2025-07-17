eb073c532795c38b7a8624cd40318a712f35b478
monifinebakery <monifinebakery@gmail.com>
2025-07-17 22:43:16 +0700

    Updated FinancialTransactionDialog.tsx

--- src/components/FinancialTransactionDialog.tsx
+++ src/components/FinancialTransactionDialog.tsx
@@ -3,6 +3,7 @@ import { toast } from 'sonner';
 import { DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
 
 interface FinancialTransactionDialogProps {
   isOpen: boolean;

d1d529629980d1662c5bf35d65e121390dd5c31f
monifinebakery <monifinebakery@gmail.com>
2025-07-20 17:21:59 +0700

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { visualizer } from 'rollup-plugin-visualizer' // <-- 1. Impor plugin
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({

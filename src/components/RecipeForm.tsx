29afeecd8b7eb9d63432ed377aa610bc3b542156
monifinebakery <monifinebakery@gmail.com>
2025-07-20 17:43:28 +0700

    Updated RecipeForm.tsx

--- src/components/RecipeForm.tsx
+++ src/components/RecipeForm.tsx
@@ -10,7 +10,7 @@ import { useBahanBaku } from "@/contexts/BahanBakuContext";
 import { useIngredientPrices } from "@/hooks/useIngredientPrices";
 import { toast } from "sonner";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
-import { useUserSettings } from "@/Contexts/UserSettingsContext"; // MODIFIED: Import useUserSettings
+import { useUserSettings } from "@/Contexts/UserSettingsContext";
 
 interface RecipeFormProps {
   initialData?: Recipe | null;

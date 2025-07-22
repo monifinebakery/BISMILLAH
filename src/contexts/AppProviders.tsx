0aad53a928068f6d1baf310ee6c95cd9c7323f67
monifinebakery <monifinebakery@gmail.com>
2025-07-22 19:37:27 +0700

    Updated AppProviders.tsx

--- src/contexts/AppProviders.tsx
+++ src/contexts/AppProviders.tsx
@@ -1,48 +1,79 @@
 // src/providers/AppProviders.tsx
 
 import React, { ReactNode } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Toaster } from 'sonner';
 
+// Import semua context provider Anda
 import { AuthProvider } from './AuthContext';
 import { UserSettingsProvider } from './UserSettingsContext';
-import { ActivityProvider } from './ActivityContext'; // Perlu di atas Financial & Order
-import { FinancialProvider } from './FinancialContext'; // Perlu di atas Order
-import { PaymentProvider } from './PaymentContext'; // Perlu di atas Promo (jika promo ada fitur berbayar) atau di posisi awal jika independen
-import { PromoProvider } from './PromoContext'; // Relatif independen dari banyak hal kecuali Auth
+import { ActivityProvider } from './ActivityContext';
+import { FinancialProvider } from './FinancialContext';
+import { PaymentProvider } from './PaymentContext';
+import { PromoProvider } from './PromoContext';
 import { BahanBakuProvider } from './BahanBakuContext';
 import { SupplierProvider } from './SupplierContext';
 import { RecipeProvider } from './RecipeContext';
 import { AssetProvider } from './AssetContext';
 import { PurchaseProvider } from './PurchaseContext';
 import { OrderProvider } from './OrderContext';
+import { FollowUpTemplateProvider } from './FollowUpTemplateContext'; // <-- Provider baru ditambahkan
 
-
+/**
+ * Komponen ini berfungsi sebagai "pembungkus" utama untuk seluruh aplikasi.
+ * Ia mengatur semua context provider dalam urutan yang benar berdasarkan dependensi,
+ * serta setup untuk routing dan notifikasi toast.
+ */
 export const AppProviders: React.FC<{ children: ReactNode }> = ({ children }) => {
   return (
-    <AuthProvider> {/* 1. Paling luar, karena hampir semua bergantung padanya */}
-      <UserSettingsProvider> {/* 2. OrderContext menggunakan settings */}
-        <ActivityProvider> {/* 3. Financial & Order Contexts menggunakan addActivity */}
-          <FinancialProvider> {/* 4. OrderContext menggunakan addFinancialTransaction */}
-            <PaymentProvider> {/* 5. Jika fitur promo bisa berbayar, PaymentProvider harus di atas PromoProvider */}
-              <PromoProvider> {/* 6. PromoProvider memerlukan AuthProvider */}
-                {/* Provider-provider lain yang mungkin kurang memiliki dependensi silang yang kompleks */}
-                <BahanBakuProvider>
-                  <SupplierProvider>
-                    <RecipeProvider>
-                      <AssetProvider>
-                        <PurchaseProvider>
-                          <OrderProvider>
-                            {children}
-                          </OrderProvider>
-                        </PurchaseProvider>
-                      </AssetProvider>
-                    </RecipeProvider>
-                  </SupplierProvider>
-                </BahanBakuProvider>
-              </PromoProvider>
-            </PaymentProvider>
-          </FinancialProvider>
-        </ActivityProvider>
-      </UserSettingsProvider>
-    </AuthProvider>
+    // Router membungkus semua agar routing berfungsi di seluruh aplikasi
+    <Router>
+      {/* 1. AuthProvider paling luar, karena hampir semua bergantung padanya */}
+      <AuthProvider>
+        {/* 2. UserSettingsProvider, mungkin dibutuhkan oleh provider lain */}
+        <UserSettingsProvider>
+          {/* 3. ActivityProvider, dibutuhkan oleh Financial & Order */}
+          <ActivityProvider>
+            {/* 4. FinancialProvider, dibutuhkan oleh Order */}
+            <FinancialProvider>
+              {/* 5. PaymentProvider & PromoProvider */}
+              <PaymentProvider>
+                <PromoProvider>
+                  {/* Provider-provider untuk manajemen data inti */}
+                  <BahanBakuProvider>
+                    <SupplierProvider>
+                      <RecipeProvider>
+                        <AssetProvider>
+                          <PurchaseProvider>
+                            {/* 6. OrderProvider */}
+                            <OrderProvider>
+                              {/* 7. FollowUpTemplateProvider dibungkus di dalam OrderProvider
+                                  sesuai catatan, karena secara konseptual terkait erat dengan order. */}
+                              <FollowUpTemplateProvider>
+                                
+                                {/* Di sinilah komponen utama aplikasi Anda (seperti <Routes>) akan dirender */}
+                                {children}
+
+                              </FollowUpTemplateProvider>
+                            </OrderProvider>
+                          </PurchaseProvider>
+                        </AssetProvider>
+                      </RecipeProvider>
+                    </SupplierProvider>
+                  </BahanBakuProvider>
+                </PromoProvider>
+              </PaymentProvider>
+            </FinancialProvider>
+          </ActivityProvider>
+        </UserSettingsProvider>
+      </AuthProvider>
+
+      {/* Komponen Toaster untuk notifikasi global, ditempatkan di luar children */}
+      <Toaster 
+        position="top-right"
+        richColors
+        closeButton
+      />
+    </Router>
   );
 };

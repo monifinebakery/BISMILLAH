d0314e32d3b665d5dc4981560b62518c93358ec8
monifinebakery <monifinebakery@gmail.com>
2025-07-24 18:47:31 +0700

    Updated ProductSelection.tsx

--- src/components/promo/ProductSelection.tsx
+++ src/components/promo/ProductSelection.tsx
@@ -3,7 +3,7 @@
 import React, { useState, useMemo } from 'react';
 import { Search, ChevronDown, Package, DollarSign, TrendingUp } from 'lucide-react';
 import { Recipe, ProductSelectionProps } from '../types';
-import { formatCurrency } from '../utils';
+import { formatCurrency } from '@/utils/currencyUtils';
 import { useSearch, useToggle } from '../hooks';
 
 interface ProductSelectionComponent extends React.FC<ProductSelectionProps> {

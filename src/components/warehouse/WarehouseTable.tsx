b32504436490a62dd918f3a574413a99f9608e1b
monifinebakery <monifinebakery@gmail.com>
2025-07-24 20:55:12 +0700

    Updated WarehouseTable.tsx

--- src/components/warehouse/WarehouseTable.tsx
+++ src/components/warehouse/WarehouseTable.tsx
@@ -15,7 +15,7 @@ import {
 } from '@/components/ui/dropdown-menu';
 import { BahanBaku } from '../../types/warehouse';
 import { formatCurrency } from '@/utils/formatUtils';
-import { formatDateForDisplay } from '@/utils/dateUtils';
+import { formatDateForDisplay } from '@/utils/dashboardUtils';
 import { cn } from '@/lib/utils';
 
 interface WarehouseTableProps {

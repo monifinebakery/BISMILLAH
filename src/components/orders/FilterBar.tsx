b457201d9666fdfbb353ad143b87d72c4a0e4c9f
monifinebakery <monifinebakery@gmail.com>
2025-07-24 20:12:38 +0700

    Updated FilterBar.tsx

--- src/components/orders/FilterBar.tsx
+++ src/components/orders/FilterBar.tsx
@@ -9,7 +9,7 @@ import { Badge } from '@/components/ui/badge';
 import { OrderFilters, DateRange } from '@/types/order';
 import { orderStatusList } from '@/constants/orderConstants';
 import { formatDateRange } from '@/utils/dashboardUtils';
-import DateRangePicker from '@/components/ui/DateRangePicker';
+import DateRangePicker from './DateRangePicker';
 
 interface FilterBarProps {
   filters: OrderFilters;

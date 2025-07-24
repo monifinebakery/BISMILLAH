7d412c621d7521c4525d11af5e077272a6507c27
monifinebakery <monifinebakery@gmail.com>
2025-07-24 18:47:45 +0700

    Updated PromoConfiguration.tsx

--- src/components/promo/PromoConfiguration.tsx
+++ src/components/promo/PromoConfiguration.tsx
@@ -3,7 +3,7 @@
 import React, { useState, useCallback } from 'react';
 import { Percent, DollarSign, Gift, AlertCircle, Info, Zap } from 'lucide-react';
 import { PromoConfigurationProps, PromoType } from '../types';
-import { formatCurrency } from '../utils';
+import { formatCurrency } from '@/utils/currencyUtils';
 import { PROMO_TYPE_LABELS, VALIDATION_RULES } from '../utils/constants';
 
 interface PromoConfigurationComponent extends React.FC<PromoConfigurationProps> {

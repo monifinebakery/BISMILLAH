f3b981b35c028032ab0ea505fab9f8fd93f6553a
monifinebakery <monifinebakery@gmail.com>
2025-07-25 18:30:42 +0700

    Updated FilterBar.tsx

--- src/components/orders/components/FilterBar.tsx
+++ src/components/orders/components/FilterBar.tsx
@@ -6,7 +6,7 @@ import { Input } from '@/components/ui/input';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { Button } from '@/components/ui/button';
 import { Badge } from '@/components/ui/badge';
-import { OrderFilters, DateRange } from '@/types/order';
+import { OrderFilters, DateRange } from './types/order';
 import { orderStatusList } from './constants/orderConstants';
 import { formatDateRange } from '@/utils/unifiedDateUtils';
 import { useIsMobile } from '@/hooks/use-mobile';  // ← Import hook yang sudah ada

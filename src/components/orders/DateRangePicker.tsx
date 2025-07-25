e727c74c534952042cdd5787a628dc8d0e3ae223
monifinebakery <monifinebakery@gmail.com>
2025-07-25 09:46:51 +0700

    Updated DateRangePicker.tsx

--- src/components/orders/DateRangePicker.tsx
+++ src/components/orders/DateRangePicker.tsx
@@ -7,7 +7,7 @@ import { Button } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
 import { useIsMobile } from '@/hooks/use-mobile';
 import { id } from 'date-fns/locale';
-import { DateRange } from '../types';
+import { DateRange } from '/types/order';
 import { formatDateRange, isValidDate, parseDate } from '@/utils/dashboardUtils';
 import DatePresets from './DatePresets';
 

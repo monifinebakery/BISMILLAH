688e203bbc3f987994b4f0ea4868a25382126f65
monifinebakery <monifinebakery@gmail.com>
2025-07-25 18:30:16 +0700

    Updated DatePresets.tsx

--- src/components/orders/components/DatePresets.tsx
+++ src/components/orders/components/DatePresets.tsx
@@ -1,7 +1,7 @@
 // src/components/orders/components/DatePresets.tsx (FIXED VERSION)
 import React from 'react';
 import { Button } from '@/components/ui/button';
-import { DateRange } from '@/types/order';
+import { DateRange } from './types/order';
 import { getDateRangePreset } from '@/utils/unifiedDateUtils';
 import { DATE_RANGE_PRESETS } from '@/constants/orderConstants';
 

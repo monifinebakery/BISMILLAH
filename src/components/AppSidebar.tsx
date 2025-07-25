c285d13325454bbbcdf1879e25fd36a4d1eabcc5
monifinebakery <monifinebakery@gmail.com>
2025-07-25 20:13:38 +0700

    Updated AppSidebar.tsx

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -46,7 +46,7 @@ import { useSupplier } from "@/contexts/SupplierContext";
 import { usePurchase } from "@/components/purchase/context/PurchaseContext";
 import { useRecipe } from "@/contexts/RecipeContext";
 import { useActivity } from "@/contexts/ActivityContext";
-import { useOrder } from "@/components/order/context/OrderContext";
+import { useOrder } from "@/components/orders/context/OrderContext";
 import { useAssets } from "@/contexts/AssetContext";
 import { useFinancial } from "@/contexts/FinancialContext";
 import { useUserSettings } from "@/contexts/UserSettingsContext";

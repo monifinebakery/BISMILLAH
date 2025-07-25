3431236cf68049094e1e9578df0f3e1104f4f1e6
monifinebakery <monifinebakery@gmail.com>
2025-07-25 18:31:26 +0700

    Updated OrdersPagination.tsx

--- src/components/orders/components/OrdersPagination.tsx
+++ src/components/orders/components/OrdersPagination.tsx
@@ -7,7 +7,7 @@ import { Button } from '@/components/ui/button';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { Label } from '@/components/ui/label';
 import { cn } from '@/lib/utils';
-import { generatePageNumbers } from '@/hooks/useOrdersPagination';
+import { generatePageNumbers } from './hooks/useOrdersPagination';
 
 export interface OrdersPaginationProps {
   // Pagination State

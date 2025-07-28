b5854331d3af69bb1e3ff40abba2f2cfdd313146
monifinebakery <monifinebakery@gmail.com>
2025-07-28 22:49:41 +0700

    Updated index.ts

--- src/components/warehouse/components/index.ts
+++ src/components/warehouse/components/index.ts
@@ -1,6 +1,21 @@
 // src/components/warehouse/components/index.ts
-// Static Components (Always Loaded)
+/**
+ * Warehouse Components Barrel Export
+ * Static components that are always loaded
+ */
+
+// Core Components
 export { default as WarehouseHeader } from './WarehouseHeader';
-export { default as WarehouseTable } from './WarehouseTable';  
+export { default as WarehouseTable } from './WarehouseTable';
 export { default as WarehouseFilters } from './WarehouseFilters';
 export { default as BulkActions } from './BulkActions';
+export { default as DialogManager } from './DialogManager';
+
+// Type-only exports for components
+export type {
+  WarehouseHeaderProps,
+  WarehouseTableProps,
+  WarehouseFiltersProps,
+  BulkActionsProps,
+  DialogManagerProps
+} from '../types';

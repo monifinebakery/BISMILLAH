74e0c5002b75865134f2288d1cb814bc917b3924
monifinebakery <monifinebakery@gmail.com>
2025-07-28 21:11:34 +0700

    Updated index.ts

---
+++ src/components/orders/components/shared/LoadingStates/index.ts
@@ -0,0 +1,5 @@
+// 🎯 8 lines - Loading state exports
+export { default as PageLoading } from './PageLoading';
+export { default as DialogLoader } from './DialogLoader';
+export { default as TableSkeleton } from './TableSkeleton';
+export { default as ButtonLoading } from './ButtonLoading';

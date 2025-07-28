756a7fe329259dca4352866ac9238a52d3c818fc
monifinebakery <monifinebakery@gmail.com>
2025-07-28 22:38:38 +0700

    Updated index.ts

---
+++ src/components/warehouse/components/index.ts
@@ -0,0 +1,6 @@
+// src/components/warehouse/components/index.ts
+// Static Components (Always Loaded)
+export { default as WarehouseHeader } from './WarehouseHeader';
+export { default as WarehouseTable } from './WarehouseTable';  
+export { default as WarehouseFilters } from './WarehouseFilters';
+export { default as BulkActions } from './BulkActions';

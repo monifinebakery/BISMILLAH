60e6b560ebd4c01ab08eecf34d854aae54ee6188
monifinebakery <monifinebakery@gmail.com>
2025-07-28 21:41:45 +0700

    Updated index.ts

---
+++ src/components/orders/context/index.ts
@@ -0,0 +1,4 @@
+// 🎯 10 lines - Context exports
+export { OrderProvider } from './OrderProvider';
+export { useOrder } from './OrderContext';
+export { default as OrderContext } from './OrderContext';

c6864f9f2acecbbc8bb24ed4e4e2dd1cd3367748
monifinebakery <monifinebakery@gmail.com>
2025-07-28 22:34:30 +0700

    Deleted index.ts

--- src/components/warehouse/context/hooks/index.ts
+++
@@ -1,5 +0,0 @@
-// src/components/warehouse/context/hooks/index.ts
-
-export { useConnectionManager } from './useConnectionManager';
-export { useNotificationDeduplicator } from './useNotificationDeduplicator';
-export { useInventoryAnalysis } from './useInventoryAnalysis';

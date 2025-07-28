4e8b5582f3a6fbab75948387303249e7aa2f1916
monifinebakery <monifinebakery@gmail.com>
2025-07-28 21:37:03 +0700

    Deleted index.ts

--- src/components/orders/components/shared/LoadingStates/index.ts
+++
@@ -1,5 +0,0 @@
-// 🎯 8 lines - Loading state exports
-export { default as PageLoading } from './PageLoading';
-export { default as DialogLoader } from './DialogLoader';
-export { default as TableSkeleton } from './TableSkeleton';
-export { default as ButtonLoading } from './ButtonLoading';

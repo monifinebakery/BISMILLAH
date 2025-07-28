1ae486999fecb715e7e44f92502a2f9aecae4547
monifinebakery <monifinebakery@gmail.com>
2025-07-28 18:33:24 +0700

    Updated index.ts

---
+++ src/components/warehouse/components/dialogs/index.ts
@@ -0,0 +1,8 @@
+// src/components/warehouse/components/dialogs/index.ts
+
+export { default as AddItemDialog } from './AddItemDialog';
+export { default as EditItemDialog } from './EditItemDialog';
+export { default as BulkEditDialog } from './BulkEditDialog';
+export { default as BulkDeleteDialog } from './BulkDeleteDialog';
+export { default as ExportDialog } from './ExportDialog';
+export { default as ImportDialog } from './ImportDialog';

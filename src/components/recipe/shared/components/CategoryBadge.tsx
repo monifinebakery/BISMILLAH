11071297862284ef36594865414d3580b03279f6
monifinebakery <monifinebakery@gmail.com>
2025-07-28 17:11:31 +0700

    Updated CategoryBadge.tsx

--- src/components/recipe/shared/components/CategoryBadge.tsx
+++ src/components/recipe/shared/components/CategoryBadge.tsx
@@ -28,36 +28,7 @@ export const CategoryBadge: React.FC<CategoryBadgeProps> = ({
   );
 };
 
-// src/components/recipe/shared/components/PriceDisplay.tsx
 
-import React from 'react';
-import { formatCurrency } from '../utils/recipeFormatters';
-
-interface PriceDisplayProps {
-  amount: number;
-  className?: string;
-  showPrefix?: boolean;
-  prefix?: string;
-  colorClass?: string;
-}
-
-export const PriceDisplay: React.FC<PriceDisplayProps> = ({ 
-  amount, 
-  className = '',
-  showPrefix = false,
-  prefix = '',
-  colorClass = ''
-}) => {
-  const defaultColorClass = amount < 0 ? 'text-red-600' : 'text-green-600';
-  const finalColorClass = colorClass || defaultColorClass;
-
-  return (
-    <span className={`font-medium ${finalColorClass} ${className}`}>
-      {showPrefix && prefix && <span className="text-xs mr-1">{prefix}</span>}
-      {formatCurrency(amount)}
-    </span>
-  );
-};
 
 // src/components/recipe/shared/components/ActionButtons.tsx
 

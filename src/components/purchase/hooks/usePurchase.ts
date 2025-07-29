2d7229a03936f2d02fac654917f3d351358ccc71
monifinebakery <monifinebakery@gmail.com>
2025-07-29 22:51:32 +0700

    Updated usePurchase.ts

---
+++ src/components/purchase/hooks/usePurchase.ts
@@ -0,0 +1,19 @@
+// src/components/purchase/hooks/usePurchase.ts
+
+import { useContext } from 'react';
+import { PurchaseContext } from '../context/PurchaseContext';
+import { PurchaseContextType } from '../types/purchase.types';
+
+/**
+ * Hook to access Purchase context
+ * This is a standalone version of the hook for better modularity
+ */
+export const usePurchase = (): PurchaseContextType => {
+  const context = useContext(PurchaseContext);
+  
+  if (context === undefined) {
+    throw new Error('usePurchase must be used within a PurchaseProvider');
+  }
+  
+  return context;
+};

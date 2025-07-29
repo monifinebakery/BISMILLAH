803937fde3ee0edeab48d6c195820a596b88946f
monifinebakery <monifinebakery@gmail.com>
2025-07-29 20:06:04 +0700

    Updated Recipes.tsx

--- src/pages/Recipes.tsx
+++ src/pages/Recipes.tsx
@@ -16,6 +16,7 @@ import RecipeTable from '@/components/recipe/components/RecipeList/RecipeTable';
 import RecipeFilters from '@/components/recipe/components/RecipeList/RecipeFilters';
 import RecipeStats from '@/components/recipe/components/RecipeList/RecipeStats';
 import { LoadingState, EmptyState } from '@/components/recipe/components/shared/LoadingState';
+import { EmptyState } from '@/components/recipe/components/shared/EmptyState';
 
 // Types
 import type { Recipe, NewRecipe } from '@/components/recipe/types';

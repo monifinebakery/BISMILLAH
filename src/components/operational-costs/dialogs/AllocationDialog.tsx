d21d7c1103b296c96906ec4a7478a35d850e841e
monifinebakery <monifinebakery@gmail.com>
2025-07-30 20:05:11 +0700

    Updated AllocationDialog.tsx

--- src/components/operational-costs/dialogs/AllocationDialog.tsx
+++ src/components/operational-costs/dialogs/AllocationDialog.tsx
@@ -42,7 +42,6 @@ const AllocationDialog: React.FC<AllocationDialogProps> = ({
               onClick={onClose}
               className="h-8 w-8"
             >
-              <X className="h-4 w-4" />
             </Button>
           </div>
         </DialogHeader>

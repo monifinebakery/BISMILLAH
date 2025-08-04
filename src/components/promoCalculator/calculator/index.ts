2bc2c0bcc5a6d6d9e474e4d11f3b06eca627e050
monifinebakery <monifinebakery@gmail.com>
2025-08-04 13:56:01 +0700

    Updated index.ts

---
+++ src/components/promoCalculator/calculator/index.ts
@@ -0,0 +1,11 @@
+// 📁 components/promoCalculator/calculator/index.ts (Calculator Components)
+export { default as PromoCalculator } from './PromoCalculator';
+export { default as PromoPreview } from './PromoPreview';
+export { default as PromoTypeSelector } from './PromoTypeSelector';
+
+// Forms - lazy loaded only
+// const BogoForm = lazy(() => import('./forms/BogoForm'));
+// const BundleForm = lazy(() => import('./forms/BundleForm'));
+// const DiscountForm = lazy(() => import('./forms/DiscountForm'));
+
+// ---

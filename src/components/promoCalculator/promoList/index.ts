0d226607d6f4e53d890c030595b2a7e14dcea025
monifinebakery <monifinebakery@gmail.com>
2025-08-04 13:56:19 +0700

    Updated index.ts

---
+++ src/components/promoCalculator/promoList/index.ts
@@ -0,0 +1,9 @@
+// 📁 components/promoCalculator/promoList/index.ts (List Components)
+export { default as PromoList } from './PromoList';
+export { default as BulkActions } from './BulkActions';
+export { default as PromoEditModal } from './PromoEditModal';
+export { default as PromoFilters } from './PromoFilters';
+export { default as PromoTable } from './PromoTable';
+export { default as PromoTableRow } from './PromoTableRow';
+
+// ---

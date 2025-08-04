d23d2bb12d9662fede79b5ca943a8f05f9646c4c
monifinebakery <monifinebakery@gmail.com>
2025-08-04 13:56:35 +0700

    Updated index.ts

---
+++ src/components/promoCalculator/hooks/index.ts
@@ -0,0 +1,7 @@
+// 📁 components/promoCalculator/hooks/index.ts (Hooks)
+export { usePromoAnalytics } from './usePromoAnalytics';
+export { usePromoCalculation } from './usePromoCalculation';
+export { usePromoForm } from './usePromoForm';
+export { usePromoList } from './usePromoList';
+
+// ---

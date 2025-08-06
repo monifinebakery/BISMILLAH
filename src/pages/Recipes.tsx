9fe321954bb6840eec4714b1252c3d942a45e595
monifinebakery <monifinebakery@gmail.com>
2025-08-06 18:25:30 +0700

    Updated Recipes.tsx

--- src/pages/Recipes.tsx
+++ src/pages/Recipes.tsx
@@ -5,6 +5,7 @@ import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { Card, CardContent } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { toast } from 'sonner';
+import { logger } from '@/utils/logger';
 
 // API and services
 import { recipeApi } from '@/components/recipe/services/recipeApi';

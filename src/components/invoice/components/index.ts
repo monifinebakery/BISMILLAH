65af303bdc5a2e5eafca0e955d2565bbadb1fd33
monifinebakery <monifinebakery@gmail.com>
2025-08-10 18:57:57 +0700

    Updated index.ts

--- src/components/invoice/components/index.ts
+++ src/components/invoice/components/index.ts
@@ -1,5 +1,8 @@
-// src/components/invoice/components/index.ts
+// Update src/components/invoice/components/index.ts
 export * from './StatusBadge';
 export * from './InvoiceHeader';
 export * from './CustomerInfo';
 export * from './ItemsTable';
+export * from './TotalsSection';
+export * from './PaymentInstructions';
+export * from './InvoiceActions';

a4551eb00bcafc5b889ab19b75859f6573750e09
monifinebakery <monifinebakery@gmail.com>
2025-08-10 21:22:15 +0700

    Updated index.ts

--- src/components/assets/components/index.ts
+++ src/components/assets/components/index.ts
@@ -5,6 +5,7 @@ export * from './AssetStatistics';
 export * from './AssetTable';
 export * from './AssetCard';
 export * from './AssetForm';
+export * from './AssetFormFields';
 export * from './AssetDeleteDialog';
 export * from './AssetActions';
 export * from './AssetConditionBadge';

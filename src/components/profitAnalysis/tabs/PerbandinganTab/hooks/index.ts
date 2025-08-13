7757385f2e547dc7a7693d80601cd69972c14073
monifinebakery <monifinebakery@gmail.com>
2025-08-13 22:32:34 +0700

    Updated index.ts

---
+++ src/components/profitAnalysis/tabs/PerbandinganTab/hooks/index.ts
@@ -0,0 +1,6 @@
+// src/components/profitAnalysis/tabs/PerbandinganTab/hooks/index.ts
+
+export { useComparisonCalculations } from './useComparisonCalculations';
+export { useBenchmarkData } from './useBenchmarkData';
+export { useCompetitiveAnalysis } from './useCompetitiveAnalysis';
+export { useImprovementPotential } from './useImprovementPotential';

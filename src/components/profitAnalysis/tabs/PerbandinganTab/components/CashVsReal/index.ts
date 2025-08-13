6a353deda5973ef78b1fbcf78a25aca982a99148
monifinebakery <monifinebakery@gmail.com>
2025-08-13 22:33:54 +0700

    Updated index.ts

---
+++ src/components/profitAnalysis/tabs/PerbandinganTab/components/CashVsReal/index.ts
@@ -0,0 +1,4 @@
+// src/components/profitAnalysis/tabs/PerbandinganTab/components/CashVsReal/index.ts
+
+export { CashVsRealComparisonCard } from './CashVsRealComparison';
+export { DataAccuracyAnalysis } from './DataAccuracyAnalysis';

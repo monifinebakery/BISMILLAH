b184187312d680a385d5b516190ed5ddfa5870ae
monifinebakery <monifinebakery@gmail.com>
2025-08-13 22:35:12 +0700

    Updated index.ts

---
+++ src/components/profitAnalysis/tabs/PerbandinganTab/components/Benchmarks/index.ts
@@ -0,0 +1,4 @@
+// src/components/profitAnalysis/tabs/PerbandinganTab/components/Benchmarks/index.ts
+
+export { IndustryBenchmarksCard } from './IndustryBenchmarks';
+export { CostRatiosAnalysis } from './CostRatiosAnalysis';

c5ca3df603fd1b650dd9a2b971c2860c1445d718
monifinebakery <monifinebakery@gmail.com>
2025-08-13 22:36:02 +0700

    Updated index.ts

---
+++ src/components/profitAnalysis/tabs/PerbandinganTab/components/Competitive/index.ts
@@ -0,0 +1,5 @@
+// src/components/profitAnalysis/tabs/PerbandinganTab/components/Competitive/index.ts
+
+export { CompetitiveTable } from './CompetitiveTable';
+export { CompetitivePositionCards } from './CompetitivePositionCards';
+export { StrategicRecommendations } from './StrategicRecommendations';

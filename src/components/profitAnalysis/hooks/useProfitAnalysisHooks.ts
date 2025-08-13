5c72fd4c74f382f92b11c7dfa6aa4f2ac3e65407
monifinebakery <monifinebakery@gmail.com>
2025-08-13 16:08:23 +0700

    Updated useProfitAnalysisHooks.ts

--- src/components/profitAnalysis/hooks/useProfitAnalysisHooks.ts
+++ src/components/profitAnalysis/hooks/useProfitAnalysisHooks.ts
@@ -4,7 +4,7 @@
 import { useState, useCallback } from 'react';
 import { useQuery, useMutation } from '@tanstack/react-query';
 import { useProfitMargin } from '../hooks/useProfitMargin';
-import { DatePeriod } from '../types/profitAnalysis';
+import { DatePeriod } from '../types';
 
 /**
  * Hook untuk dialog state management

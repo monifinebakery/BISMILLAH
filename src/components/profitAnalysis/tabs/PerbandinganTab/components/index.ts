1c68db37bbaf6f4d7db8b60715314f1c9c89e3db
monifinebakery <monifinebakery@gmail.com>
2025-08-13 22:37:03 +0700

    Updated index.ts

---
+++ src/components/profitAnalysis/tabs/PerbandinganTab/components/index.ts
@@ -0,0 +1,15 @@
+// src/components/profitAnalysis/tabs/PerbandinganTab/components/index.ts
+
+export { ComparisonViewSelector } from './ComparisonViewSelector';
+
+// CashVsReal exports
+export * from './CashVsReal';
+
+// Benchmarks exports  
+export * from './Benchmarks';
+
+// Competitive exports
+export * from './Competitive';
+
+// Improvement exports
+export * from './Improvement';

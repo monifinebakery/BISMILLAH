9b6331ed205beb3d0d469b8f45259f8d55c68f07
monifinebakery <monifinebakery@gmail.com>
2025-08-14 08:47:25 +0700

    Updated index.ts

---
+++ src/components/profitAnalysis/constants/index.ts
@@ -0,0 +1,14 @@
+// 2. src/components/profitAnalysis/constants/index.ts
+// ==============================================
+
+export { 
+  PROFIT_CONSTANTS,
+  REVENUE_CATEGORIES,
+  CHART_CONFIG
+} from './profitConstants';
+
+export default {
+  PROFIT_CONSTANTS,
+  REVENUE_CATEGORIES,
+  CHART_CONFIG
+};

4c78f318f8251f997fa23b4c00b449f53d6f8fd0
monifinebakery <monifinebakery@gmail.com>
2025-08-20 13:17:25 +0700

    fix: definisikan dan impor DEVICE_BREAKPOINTS

---
+++ src/utils/constants.ts
@@ -0,0 +1,71 @@
+// src/utils/constants.ts
+// Placeholder constant definitions referenced in utils/index.ts
+
+export const PROMO_TYPES = {};
+export const PROMO_TYPE_LABELS = {};
+export const VALIDATION_RULES = {};
+export const MARGIN_THRESHOLDS = {};
+export const MARGIN_COLORS = {};
+export const UI_CONFIG = {};
+export const BREAKPOINTS = {};
+export const STATUS_TYPES = {};
+export const CHART_CONFIG = {};
+export const INPUT_PATTERNS = {
+  EMAIL: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+  PHONE: /^\d+$/,
+};
+export const FILE_CONFIG = {};
+export const API_CONFIG = {};
+export const PERFORMANCE_THRESHOLDS = {};
+export const SEARCH_CONFIG = {};
+export const DATE_FORMATS = {};
+export const LOCALES = {};
+export const CURRENCY_CONFIG = {};
+export const SECURITY_CONFIG = {};
+export const ANALYTICS_EVENTS = {};
+export const FEATURE_FLAGS = {};
+export const LOADING_MESSAGES = {};
+export const ERROR_MESSAGES = {};
+export const SUCCESS_MESSAGES = {};
+export const DEFAULT_VALUES = {};
+export const CALCULATION_CONSTANTS = {};
+export const ANIMATION_DURATIONS = {};
+export const DEVICE_BREAKPOINTS = {
+  MOBILE: 768,
+  TABLET: 1024,
+};
+export const FILTER_OPTIONS = {};
+export const CONSTANTS = {};
+
+export default {
+  PROMO_TYPES,
+  PROMO_TYPE_LABELS,
+  VALIDATION_RULES,
+  MARGIN_THRESHOLDS,
+  MARGIN_COLORS,
+  UI_CONFIG,
+  BREAKPOINTS,
+  STATUS_TYPES,
+  CHART_CONFIG,
+  INPUT_PATTERNS,
+  FILE_CONFIG,
+  API_CONFIG,
+  PERFORMANCE_THRESHOLDS,
+  SEARCH_CONFIG,
+  DATE_FORMATS,
+  LOCALES,
+  CURRENCY_CONFIG,
+  SECURITY_CONFIG,
+  ANALYTICS_EVENTS,
+  FEATURE_FLAGS,
+  LOADING_MESSAGES,
+  ERROR_MESSAGES,
+  SUCCESS_MESSAGES,
+  DEFAULT_VALUES,
+  CALCULATION_CONSTANTS,
+  ANIMATION_DURATIONS,
+  DEVICE_BREAKPOINTS,
+  FILTER_OPTIONS,
+  CONSTANTS,
+};
+

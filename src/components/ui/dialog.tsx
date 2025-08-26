1aed7bd87baee2e570ce4e950d5366490412ccbc
mymac <monifinebakery@gmail.com>
2025-08-26 21:27:07 +0700

    angel e

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -53,7 +53,7 @@ const DialogContent = React.forwardRef<
       <DialogPrimitive.Content
         ref={ref}
         className={cn(
-          "fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-4 sm:p-6 duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 rounded-xl mx-12 sm:mx-16 md:mx-auto max-h-[85vh] sm:max-h-[80vh] overflow-auto shadow-xl",
+          "fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-4 sm:p-6 duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 rounded-xl mx-20 sm:mx-24 md:mx-auto max-h-[85vh] sm:max-h-[80vh] overflow-auto shadow-xl",
           className
         )}
         {...mergedProps}
